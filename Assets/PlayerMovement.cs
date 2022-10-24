607940ffeef8410127c36ffce18e3545e6a3f928
FerchichiXIII <anasferchichi22@gmail.com>
2022-10-24 14:52:00 +0100

    update player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     private Animator anim;
     private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 10f;
+    // if use serializeField i can update the jump force and speed in the unity .
     // Start is called before the first frame update
     private void Start()
     {
@@ -21,13 +24,13 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
         // victor3 is more importans in 3D game and victor2 2D games.
         // victor3 :(x, y, z) and victor2 :(x, y)
         if (Input.GetButtonDown("Jump"))// this code for jump
         {
 
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
         UpdateAnimationUpdate();
     }
@@ -36,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
         if (dirX > 0f) // if player walk to the rhit
         {
             anim.SetBool("running", true);
+            sprite.flipX = false;
         }
         else if (dirX < 0f) // if player walk to the left
         {
